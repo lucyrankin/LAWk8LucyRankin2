ba9f6c7a3392e7adf99f671d7e1dc3bec9fd3f8d
lucyrankin <79422779+lucyrankin@users.noreply.github.com>
2021-03-31 20:04:29 -0500

    take two

---
+++ LAWk8LucyRankin.appstudio/forms/mobileNav/mobileNav.js
@@ -0,0 +1,22 @@
+/*
+hmbMenu.onclick = function(s) {
+  if (typeof(s) == "object") {
+    return
+  } else {
+    switch (s) {
+      case "Dessert Voting":
+        ChangeForm(dessertVoting)
+        break
+      case "Describe You":
+        ChangeForm(describeYou)
+        break
+      case "Favorite Exercises":
+        ChangeForm(favExercises)
+        break
+      case "Mobile Nav":
+        ChangeForm(mobileNav)
+        break
+    }
+  }
+}
+*/
